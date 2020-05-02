419e5ec41e8e1b035ae63e61247d95c541f61e72
patodekaz <pato@dekaz.cl>
2020-05-02 18:58:12 -0400

    Updates

---
+++ precache-manifest.6bf2fa96c97e579e16bc91bc32e4ccf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb38bb43a83c613f14c95426880d639d",
+    "url": "/fetchmovieapi/index.html"
+  },
+  {
+    "revision": "78e3637eaa430ebcff90",
+    "url": "/fetchmovieapi/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5e15bca70962190e2cdf",
+    "url": "/fetchmovieapi/static/css/main.26b882fa.chunk.css"
+  },
+  {
+    "revision": "78e3637eaa430ebcff90",
+    "url": "/fetchmovieapi/static/js/2.3205503c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fetchmovieapi/static/js/2.3205503c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e15bca70962190e2cdf",
+    "url": "/fetchmovieapi/static/js/main.9d5451ae.chunk.js"
+  },
+  {
+    "revision": "a7f7bdd085a3e5c69a09",
+    "url": "/fetchmovieapi/static/js/runtime-main.224d34d4.js"
+  }
+]);
